5d4505f6f163e37cfe55a257a58d4debb813b1fb
Hurpani <henri.urpani@gmail.com>
2020-08-20 16:48:30 +1000

    Prototype making queries using Spotify's Web API

---
+++ program.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+########################################################################################################################
+#   Create a private.py file, which contains a string variable named "token", containing an OAuth Token:               #
+#   playlist-modify-private for your Spotify account.                                                                  #
+########################################################################################################################
+
+from private import token
+endpoint: str = "https://api.spotify.com/v1/recommendations"
+
+# Filters for Spotify.
+limit: int = 10
+seed_genres: str = "pop"
+market: str = "AU"
+
+query: str = f"{endpoint}?limit={limit}&seed_genres={seed_genres}&market={market}"
+response = requests.get(query, headers={"Content-Type": "application/json", "Authorization": f"Bearer {token}"})
+
+uris: [] = []
+for (i, j) in enumerate(response.json()["tracks"]):
+    uris.append(j["uri"])
+    print(f"{i + 1}) \"{j['name']}\" by {j['artists'][0]['name']}")
+
