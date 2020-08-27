2192f7ab6589062891f5e67c05ed9373e73127cf
Hurpani <henri.urpani@gmail.com>
2020-08-27 16:32:55 +1000

    Completed the mock-up playlist generator

--- program.py
+++ program.py
@@ -6,12 +6,24 @@ import json
 #   playlist-modify-private for your Spotify account.                                                                  #
 ########################################################################################################################
 
-from private import token
+from private import token, user_id
 endpoint: str = "https://api.spotify.com/v1/recommendations"
 
+
+# Report available genres.
+response_genres = requests.get("https://api.spotify.com/v1/recommendations/available-genre-seeds",
+                               headers={"Content-Type": "application/json",
+                                        "Authorization": f"Bearer {token}"})
+print(response_genres.json())
+
+
+
+
+
+
 # Filters for Spotify.
-limit: int = 10
-seed_genres: str = "pop"
+limit: int = 30
+seed_genres: str = "ambient"#"electronic,edm"
 market: str = "AU"
 
 query: str = f"{endpoint}?limit={limit}&seed_genres={seed_genres}&market={market}"
@@ -24,8 +36,27 @@ for (i, j) in enumerate(response.json()["tracks"]):
 
 print()
 
-# Report available genres.
-response_genres = requests.get("https://api.spotify.com/v1/recommendations/available-genre-seeds",
-                               headers={"Content-Type": "application/json",
-                                        "Authorization": f"Bearer {token}"})
-print(response_genres.json())
+playlist_name: str = "Auto-generated Playlist"
+playlist_desc: str = "A generic playlist"
+# Creating a playlist:
+request_body = json.dumps(
+    {
+        "name": playlist_name,
+        "description": playlist_desc,
+        "public": False
+    }
+)
+
+playlist_endpoint: str = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+response_playlist_gen = requests.post(url=playlist_endpoint, data=request_body,
+                                      headers={"Content-Type": "application/json", "Authorization": "Bearer " + token})
+print(response_playlist_gen.status_code)
+playlist_id: str = response_playlist_gen.json()["id"]
+
+songs_to_playlist_endpoint: str = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+request_body = json.dumps({
+          "uris" : uris
+        })
+response = requests.post(url=songs_to_playlist_endpoint, data=request_body,
+                         headers={"Content-Type": "application/json", "Authorization": f"Bearer {token}"})
+print(response.status_code)
